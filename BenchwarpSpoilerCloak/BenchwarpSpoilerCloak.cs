9db7dc5754f2d7e07ef3e3b4a6420e20ddc62354
Diogo Pinela <diogoid7400@gmail.com>
2022-06-28 22:26:39 +0100

    Initialize mod

---
+++ BenchwarpSpoilerCloak/BenchwarpSpoilerCloak.cs
@@ -0,0 +1,13 @@
+using Modding;
+
+namespace BenchwarpSpoilerCloak
+{
+    public class BenchwarpSpoilerCloak : Mod
+    {
+        public override void Initialize()
+        {
+        }
+
+        public override string GetVersion() => "1.0";
+    }
+}
