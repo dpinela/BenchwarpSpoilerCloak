26137d0ff31b1a19dc2533d8a21fe8f1401590c5
Diogo Pinela <diogoid7400@gmail.com>
2022-06-29 04:43:19 +0100

    Add actual spoiler-cloaking code

--- BenchwarpSpoilerCloak/BenchwarpSpoilerCloak.cs
+++ BenchwarpSpoilerCloak/BenchwarpSpoilerCloak.cs
@@ -2,10 +2,26 @@ using Modding;
 
 namespace BenchwarpSpoilerCloak
 {
-    public class BenchwarpSpoilerCloak : Mod
+    public class BenchwarpSpoilerCloak : Mod, ITogglableMod
     {
         public override void Initialize()
         {
+            Benchwarp.Events.OnGetBenchName += HideUnvisitedBenchNames;
+            Benchwarp.Events.OnGetBenchSceneName += HideUnvisitedBenchNames;
+        }
+
+        public void Unload()
+        {
+            Benchwarp.Events.OnGetBenchName -= HideUnvisitedBenchNames;
+            Benchwarp.Events.OnGetBenchSceneName -= HideUnvisitedBenchNames;
+        }
+
+        private void HideUnvisitedBenchNames(Benchwarp.Bench bench, ref string name)
+        {
+            if (!bench.HasVisited())
+            {
+                name = "???";
+            }
         }
 
         public override string GetVersion() => "1.0";
